08b664927fc246363f4c4027849329e6a378e76e
Zaid Abeaid <zaid2016404045@gmail.com>
2026-05-01 01:39:22 +0300

    Force admin user credentials

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.models import User
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,3 +13,16 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    
+    
+    # كود للتأكد من وجود المستخدم وصلاحياته وكلمة مروره على السيرفر
+try:
+    # ابحث عن المستخدم أو أنشئه إذا لم يكن موجوداً
+    user, created = User.objects.get_or_create(username='zaid_admin')
+    user.set_password('Zaid@2026') # ضع كلمة مرور قوية
+    user.is_superuser = True
+    user.is_staff = True
+    user.save()
+except Exception as e:
+    print(f"Error: {e}")
