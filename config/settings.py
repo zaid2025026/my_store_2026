db48e768c66fa6037de2dc95f0832a0f2ba16ac7
Zaid Abeaid <zaid2016404045@gmail.com>
2026-05-02 17:12:12 +0300

    Configure media files for production

--- config/settings.py
+++ config/settings.py
@@ -139,4 +139,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CART_SESSION_ID = 'cart'
+WHITENOISE_INDEX_FILE = True
 
