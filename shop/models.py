48988bf3232cabbd9bcde694b45995e12071d517
Zaid Abeaid <zaid2016404045@gmail.com>
2026-05-02 22:24:06 +0300

    Simplify image upload path

--- shop/models.py
+++ shop/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image = models.ImageField(upload_to='products/', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
